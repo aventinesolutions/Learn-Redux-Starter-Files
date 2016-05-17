9530aef3af53dc44a395daa2f72b869f36f5f6e0
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2016-05-17 21:01:16 +0200

    1592057-119759479 :bear: hello world render
    As a Developer, I want to do Chapter 2 of the course so I learn about Redux.

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/style.styl';
+
+render(<p>hello world</p>, document.getElementById('root'));
