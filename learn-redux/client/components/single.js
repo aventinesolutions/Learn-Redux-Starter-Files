4254711150b9ec9977cb2dcda52cb2bccaae1045
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2016-05-18 17:11:05 +0200

    1592057-119827935 :bear: routes with React Router
    As a Developer, I want to do Chapter 4 so that I can learn Redux.

--- learn-redux/client/components/single.js
+++ learn-redux/client/components/single.js
@@ -4,7 +4,7 @@ const Single = React.createClass({
   render() {
     return (
       <div className="single-photo">
-        I'm the Single Photo.
+        I&apos;m the Single Photo.
       </div>
     );
   }
